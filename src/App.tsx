6d463c7f1b7b24d64e60a096f3e041863c8172a5
its-kayes <kayes.ek8@gmail.com>
2022-09-14 19:54:36 +0600

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Products from './components/PaymentSSL/Products';
 
 function App() {
   return (
     <section>
-      <p className='text-4xl'> Payment System !! </p>
+      {/* <p className='text-4xl'> Payment System !! </p> */}
+      <Products />
     </section>
   );
 }
